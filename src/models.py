d2bd6e88a51d9dd9d2d835dae7ed64902ba9fe84
sumpfel <christof.schaffer@schaffer-se.at>
2026-05-28 12:32:08 +0200

    added models: user and user settings

--- src/models.py
+++ src/models.py
@@ -1,2 +1,16 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, VARCHAR
 from database import Base
+
+class DB_user(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    user_settings_id = privileges = Column(Integer, notnull=True)
+    username = Column(String, unique=True, notnull=True)
+    password = Column(String, notnull=True)
+    privileges = Column(Integer, notnull=True)
+
+class DB_user_settings(Base):
+    __tablename__ = ("usersettings")
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    jsonsettings = Column(VARCHAR(1000))
